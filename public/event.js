290ba1aa64e5ba753f58e256a00465f3d4bca186
thethinh <nguuyenthethinh16011998@gmail.com>
2019-11-28 23:00:15 +0700

    edit a bit front-end

--- public/event.js
+++
@@ -1,6 +0,0 @@
-//Code input text trong ô search box vẫn hiện khi click search
-var input = document.getElementById('searchbox');
-input.addEventListener('change',function(){
-    sessionStorage.setItem('draft',input.value);
-});
-input.value = sessionStorage.getItem('draft'); 
