1cf760b7992c212fcf762d2eef2104103540107b
thethinh <nguuyenthethinh16011998@gmail.com>
2019-12-04 20:47:19 +0700

    completed code create_user validate , layout, add file controller, routes. This lesson, i studied so much

--- index.js
+++ index.js
@@ -1,54 +1,20 @@
 var express = require('express');
 var app = express();
 var port = 1616;
-var low = require('lowdb');
-var FileSync = require('lowdb/adapters/FileSync');
-var adapter = new FileSync('db.json');
-var shortid = require('shortid');
+const bodyParser = require('body-parser')
 
-db = low(adapter);
- db.defaults({users: [] }).write();
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+var indexRoute = require('./routes/index.route');
+
 app.set('view engine','pug');
 app.set('views','./views');
 
 app.use(express.static('public'));
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use('/',indexRoute);
 
-app.get('/',(req,res)=>{
-    res.render('index',{
-        users: db.get('users').value()
-    });
-});
-
-app.get('/search',(req,res)=>{
-    var q = req.query.q;
-    var matchussers = db.get('users').value().filter(function(user){
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
-    });
-    res.render('index',{
-        users: matchussers
-    });
-});
-
-app.get('/create-user',(req,res)=>{
-    res.render('create_user');
-});
-
-app.post('/create-user',(req,res)=>{
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect('/');
-});
-
-app.get('/:id',(req,res)=>{
-    var id = req.params.id;
-    var user = db.get('users').find({id: id}).value();
-    res.render('view_user',{
-        user: user
-    });
-})
 app.listen(port,()=>{
     console.log('Sever listening on port'+port);
 });
