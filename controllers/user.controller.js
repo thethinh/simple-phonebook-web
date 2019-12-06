e9c691454f2b851dbb7f75b04cc45d3d884fa2a4
thethinh <nguuyenthethinh16011998@gmail.com>
2019-12-06 13:39:45 +0700

    the first complete login feature

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,3 +3,48 @@ var db = require('../db');
 module.exports.getLogin = (req,res)=>{
     res.render('user/login');
 }
+
+module.exports.postLogin = (req,res)=>{
+    var email = req.body.email;
+    var password = req.body.password;
+    var user = db.get('users').find({email: email}).value();
+
+    if(!user){
+        res.render('user/login',{
+            errors:[
+                'User does not not exist.'
+            ],
+            values: req.body
+        });
+        return;
+    }
+
+    if(user.password !== password){
+        res.render('user/login',{
+            errors:[
+                'Wrong password.'
+            ],
+            values: req.body
+        });
+        return;
+    }
+
+    res.redirect('../user/index');
+}
+
+module.exports.getIndex = (req,res)=>{
+    res.render('user/index',{
+        users: db.get('users').value()
+    });
+}
+
+module.exports.getSearch = (req,res)=>{
+    var q = req.query.q;
+    var matchussers = db.get('users').value().filter(function(user){
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
+    });
+    res.render('user/index',{
+        users: matchussers,
+        value: req.query
+    });
+}
--- routes/user.route.js
+++ routes/user.route.js
@@ -5,5 +5,10 @@ var controller = require('../controllers/user.controller')
 //code for user
 router.get('/login',controller.getLogin);
 
+router.get('/index',controller.getIndex);
+
+router.get('/search',controller.getSearch);
+
+router.post('/login',controller.postLogin);
 //export
 module.exports = router;
