87de7ff06f14bd2ad5a3a34c7c27050df2ab751a
ThinhNguyen <39476468+thethinh@users.noreply.github.com>
2019-12-08 21:15:58 +0700

    Revert "Feature/thinh"

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 var express = require('express');
 var app = express();
 var port = 1616;
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use(cookieParser())
-
-var indexRoute = require('./routes/index.route');
-var userRoute = require('./routes/user.route');
-var loginRoute = require('./routes/login.route');
-var authMiddlewares = require('./middlewares/auth.middlewares');
-
-
+db = low(adapter);
+ db.defaults({users: [] }).write();
 app.set('view engine','pug');
 app.set('views','./views');
 
 app.use(express.static('public'));
 
-app.use('/',indexRoute);
-app.use('/user',authMiddlewares.requireAuth,userRoute);
-app.use('/auth',loginRoute);
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+app.get('/',(req,res)=>{
+    res.render('index',{
+        users: db.get('users').value()
+    });
+});
+
+app.get('/search',(req,res)=>{
+    var q = req.query.q;
+    var matchussers = db.get('users').value().filter(function(user){
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
+    });
+    res.render('index',{
+        users: matchussers
+    });
+});
+
+app.get('/create-user',(req,res)=>{
+    res.render('create_user');
+});
+
+app.post('/create-user',(req,res)=>{
+    db.get('users').push(req.body).write();
+    res.redirect('/');
+});
 app.listen(port,()=>{
     console.log('Sever listening on port'+port);
 });
--- routes/login.route.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var controller = require('../controllers/login.controller')
-
-router.get('/login',controller.getLogin);
-
-router.post('/login',controller.postLogin);
-
-module.exports = router
